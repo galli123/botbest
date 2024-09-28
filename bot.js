07682f00bb9853433db035002eac185308693fb4
galli123 <49342329+galli123@users.noreply.github.com>
2024-09-28 09:14:58 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -64,29 +64,21 @@ async function checkCargoes() {
             ['Коледино', 'Казань', 'Электросталь', 'Краснодар', 'Тула'].includes(element.warehouseName)
         );
 
-// Отправка новых данных подписанным пользователям
-subscribers.forEach(chatId => {
-    if (!sentItems[chatId]) {
-        sentItems[chatId] = [];
-    }
-    filteredItems.forEach(item => {
-        // Проверяем, был ли уже отправлен элемент с таким же date и warehouseID
-        let existingItem = sentItems[chatId].find(sentItem => sentItem.date === item.date && sentItem.warehouseID === item.warehouseID);
-
-        if (existingItem) {
-            // Если новый коэффициент ниже — отправляем сообщение
-            if (item.coefficient < existingItem.coefficient) {
-                let message = `Дата: ${formatDate(item.date)} \nКоэффициент: ${item.coefficient} \nСклад: ${item.warehouseName} \nТип коробки: ${item.boxTypeName}`;
-                bot.telegram.sendMessage(chatId, message);
-                console.log(message);
+        // Отправка новых данных подписанным пользователям
+        subscribers.forEach(chatId => {
+            if (!sentItems[chatId]) {
+                sentItems[chatId] = [];
             }
-            // В любом случае обновляем данные (сохраняем новые значения)
-            existingItem.coefficient = item.coefficient;
-        } else {
-            // Если это новый элемент, сразу отправляем сообщение
-            let message = `Дата: ${formatDate(item.date)} \nКоэффициент: ${item.coefficient} \nСклад: ${item.warehouseName} \nТип коробки: ${item.boxTypeName}`;
-            bot.telegram.sendMessage(chatId, message);
-            console.log(message);
+            // Фильтрация новых элементов, которые еще не отправлялись
+            let newItems = filteredItems.filter(item => 
+                !sentItems[chatId].some(sentItem => sentItem.date === item.date && sentItem.warehouseID === item.warehouseID)
+            );
+            if (newItems.length > 0) {
+                newItems.forEach(item => {
+                    let message = `Дата: ${formatDate(item.date)} \nКоэффициент: ${item.coefficient} \nСклад: ${item.warehouseName} \nТип коробки: ${item.boxTypeName}`;
+                    bot.telegram.sendMessage(chatId, message);
+                    console.log(message)
+                });
 
             // Добавляем элемент в список отправленных
             sentItems[chatId].push(item);
